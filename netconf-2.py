53695b02ee55f40869919ba843d12a8794284358
TakeOff <tfakeye7@gmail.com>
2025-02-27 17:17:28 +0100

    new python files

---
+++ netconf-2.py
@@ -0,0 +1,12 @@
+from ncclient import manager
+
+
+router = {"host": "sbx-nxos-mgmt.cisco.com", "port": "22", "username": "admin", "password": "Admin_1234!"}
+
+with manager.connect(host=router["host"], port=router["port"], username=router["username"], password=router["password"], hostkey_verify=False) as m:
+    for capability in m.server_capabilities:
+        print('*' * 50)
+        print(capability)
+    m.close_session()
+
+
