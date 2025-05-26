2be7177a4d4c8fb27e0b33877ba9ac7a6946137c
Take-Off-7 <tfakeye7@gmail.com>
2025-05-26 15:28:08 +0100

    Removed all plaintext password/token/key

--- netconf-1.py
+++ netconf-1.py
@@ -2,7 +2,8 @@ from ncclient import manager
 
 
 router = {"host": "ios-xe-mgmt-latest.cisco.com", "port": "10000", 
-          "username": "developer", "password": "Cisco12345"}
+          "username": "developer", "password": #(your_password_goes_here)
+         }
 
 with manager.connect(host=router["host"], port=router["port"], username=router["username"], password=router["password"], hostkey_verify=False) as m:
     m.close_session()
