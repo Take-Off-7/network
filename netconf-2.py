7b2907b2ce366afcb63050c42d63671d9dff84f1
Take-Off-7 <tfakeye7@gmail.com>
2025-05-26 15:28:39 +0100

    Removed all plaintext password/token/key

--- netconf-2.py
+++ netconf-2.py
@@ -1,7 +1,8 @@
 from ncclient import manager
 
 
-router = {"host": "sbx-nxos-mgmt.cisco.com", "port": "22", "username": "admin", "password": "Admin_1234!"}
+router = {"host": "sbx-nxos-mgmt.cisco.com", "port": "22", "username": "admin", "password": #(your_password_goes_here)
+         }
 
 with manager.connect(host=router["host"], port=router["port"], username=router["username"], password=router["password"], hostkey_verify=False) as m:
     for capability in m.server_capabilities:
